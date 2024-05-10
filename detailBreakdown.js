5f9961e0c7d241557f89b38fa695a60b0424db96
reneewrysinski <reneewrysinski@gmail.com>
2024-05-10 01:09:25 -0700

    turn into resistor quiz

--- detailBreakdown.js
+++ detailBreakdown.js
@@ -1,5 +1,5 @@
 const QNum = sessionStorage.getItem('QNum').split(",");
-const Q = sessionStorage.getItem('Q').split(",");
+const Q = sessionStorage.getItem('Q').split("%%%");
 const userA = sessionStorage.getItem('userA').split(",");
 const trueA = sessionStorage.getItem('trueA').split(",");
 const isTrue = sessionStorage.getItem('isTrue').split(",");
@@ -8,6 +8,7 @@ const eb = document.querySelector('.error-breakdown')
 const output = []
 
 console.log(isTrue);
+console.log(Q);
 
 for (let i=0; i<Q.length; i++){
   
@@ -16,7 +17,18 @@ for (let i=0; i<Q.length; i++){
       `
       <div class="a-${isTrue[i]}">
       <h3>Question ${QNum[i]}</h3>
-      <p>${Q[i]} = ${userA[i]}</p>
+      <svg
+      version="1.2"
+      x="0"
+      y="0"
+      width="1.5in"
+      viewBox="0 0 39.397998 9.4490009"
+      id="svg1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:svg="http://www.w3.org/2000/svg">
+      ${Q[i]}
+      </svg>
+      <p>your answer: ${userA[i]} &Omega;</p>
       </div>
       `
     )
@@ -26,8 +38,19 @@ for (let i=0; i<Q.length; i++){
       `
       <div class="a-${isTrue[i]}">
       <h3>Question ${QNum[i]}</h3>
-      <p>${Q[i]} ≠ ${userA[i]}</p>
-      <p>correct answer: ${trueA[i]}</p>
+      <svg
+      version="1.2"
+      x="0"
+      y="0"
+      width="1.5in"
+      viewBox="0 0 39.397998 9.4490009"
+      id="svg1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:svg="http://www.w3.org/2000/svg">
+      ${Q[i]}
+      </svg>
+      <p>your answer: ${userA[i]} &Omega;<br>
+      correct answer: ${trueA[i]} &Omega;</p>
       </div>
       `  
     )
--- infinity game.js
+++ infinity game.js
@@ -30,58 +30,201 @@ let trueA = [];
 let isTrue = [];
 
 // variables for timer
-let second = 00;
-let minute = 00;
-let hour = 00;
+let second = 0;
+let minute = 0;
+let hour = 0;
 let elapsed;
+
+// constants for E-series values
+const seriesValues = {
+  'e3': [10, 22, 47], // 40% tolerance
+  'e6': [10, 15, 22, 33, 47, 68], // 20% tolerance
+  'e12': [10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82], // 10% tolerance
+  'e24': [10, 11, 12, 13, 15, 16, 18, 20, 22, 24, 27, 30, 
+          33, 36, 39, 43, 47, 51, 56, 62, 68, 75, 82, 91], // 5% tolerance
+  'e48': [100, 105, 110, 115, 121, 127, 133, 140, 147, 154, 
+          162, 169, 178, 187, 196, 205, 215, 226, 237, 249, 
+          261, 274, 287, 301, 316, 332, 348, 365, 383, 402, 
+          422, 442, 464, 487, 511, 536, 562, 590, 619, 649, 
+          681, 715, 750, 787, 825, 866, 909, 953] // 2% tolerance
+};
+const multExps = [9, 8, 7, 6, -2, 5, 4, -1, 3, 2, 1, 0];
+
+// set up band objects to be customized by updateResistor()
+const body = document.getElementById("body");
+const band1 = document.getElementById("band_1");
+const tol = document.getElementById("band_tol");
+const metal_reflex1 = document.getElementById("metal_reflex1");
+const metal_reflex2 = document.getElementById("metal_reflex2");
+const metal_shadow = document.getElementById("metal_shadow");
+
+const band2_4 = document.getElementById("4band_2");
+const mult_4 = document.getElementById("4band_mult");
+const mult_r_4 = document.getElementById("4band_mult_reflex");
+const mult_s_4 = document.getElementById("4band_mult_shadow");
+
+const band2_5 = document.getElementById("5band_2");
+const band3_5 = document.getElementById("5band_3");
+const mult_5 = document.getElementById("5band_mult");
+const mult_r_5 = document.getElementById("5band_mult_reflex");
+const mult_s_5 = document.getElementById("5band_mult_shadow");
+
+// Parse URL parameters to get the selected series
+const urlParams = new URLSearchParams(window.location.search);
+const series = urlParams.get('series');
 
 // FUNCTIONS
 
-// creates a random basic math question
-function genQuestion(){
-  let question, answer;  
-  let numbers = [];
-  for (let i = 0; i <= 20; i++){
-    numbers.push(i)  
-  };
-  let operators = ['a','s','m','d'];
-  // randomly generate operator and terms
-  let ranOp = operators[Math.floor(Math.random() * operators.length)]
-  let ranX = numbers[Math.floor(Math.random() * numbers.length)]
-  let ranY = numbers[Math.floor(Math.random() * numbers.length)]
-  
-  // generate question and answer
-  if ((ranOp == 'a') & (Number.isInteger(ranX + ranY))){
-      question = `${ranX} + ${ranY}`
-      answer = ranX+ranY
+// generate random resistance from chosen series
+function genResistance() {
+  let randomIndex = Math.floor(Math.random() * seriesValues[series].length);
+  let randomMult = multExps[Math.floor((Math.random() ** (1 / 1.75)) * (multExps.length))];
+  // console.log(randomMult);
+  let sigFigs = seriesValues[series][randomIndex];
+  let answer;
+  answer = sigFigs * Math.pow(10, randomMult);
+  return [sigFigs, randomMult];
+}
+
+function updateResistor([sigFigs, randomMult]){
+    // define color codes for each band
+    const colorMap = {
+      '0': '#000000', // Black
+      '1': '#8A3D06', // Brown
+      '2': '#C40808', // Red
+      '3': '#FF4D00', // Orange
+      '4': '#FFD500', // Yellow
+      '5': '#00A33D', // Green
+      '6': '#005EB6', // Blue
+      '7': '#8210D2', // Violet
+      '8': '#8C8C8C', // Grey
+      '9': '#FFFFFF', // White
+      '-1': '#AD9F4E', // Gold
+      '-2': '#C0C0C0', // Silver
+    };
+
+    const tolColors = {
+      '0.05%': '#8C8C8C', // Grey
+      '0.1%': '#8210D2', // Violet
+      '0.25%': '#005EB6', // Blue
+      '0.5%': '#00A33D', // Green
+      '1%': '#8A3D06', // Brown
+      '2%': '#C40808', // Red
+      '3%': '#FF4D00', // Orange
+      '4%': '#FFD500', // Yellow
+      '5%': '#AD9F4E', // Gold
+      '10%': '#C0C0C0', // Silver
+      '20%': 'rgba(0, 0, 0, 0)', // no band
+      '40%': 'rgba(0, 0, 0, 0)', // no band
+    };
+
+    const tolMap = {
+      'e3': '40%',
+      'e6': '20%', 
+      'e12': '10%',
+      'e24': '5%',
+      'e48': '2%',
+    };
+    let tolerance = tolMap[series];
+    
+    // split the value into separate digits (e.g., 100 -> [1, 0, 0])
+    let digits = sigFigs.toString().split('').map(Number);
+
+    // apply colors to bands based on their digits and set unused bands to transparent
+    tol.setAttribute('fill', tolColors[tolerance]);
+    metal_reflex1.setAttribute('fill', tolColors[tolerance]);
+    metal_reflex1.setAttribute('opacity', 0.75);
+    if ( tolerance == '5%' || tolerance == '10%' ) { 
+      metal_reflex1.setAttribute('filter', 'brightness(100%) saturate(1000%) contrast(200%)');
+      metal_reflex2.setAttribute('opacity', 0.5);
+      metal_shadow.setAttribute('opacity', 0.4);
+    } else {
+      metal_reflex1.setAttribute('filter', 'brightness(100%) saturate(100%) contrast(100%)');
+      metal_reflex2.setAttribute('opacity', 0);
+      metal_shadow.setAttribute('opacity', 0);
     }
-  else if ((ranOp == 's') & (Number.isInteger(ranX - ranY))){
-      question = `${ranX} - ${ranY}`
-      answer = ranX-ranY
-    }
-  else if ((ranOp == 'm') & (Number.isInteger(ranX * ranY))){
-      question = `${ranX} x ${ranY}`
-      answer = ranX*ranY
-    }
-  else if ((ranY != 0) & (ranY != 1) & Number.isInteger(ranX/ranY)){
-      question = `${ranX} / ${ranY}`
-      answer = ranX/ranY
-    }
-  return [question,answer];
+    band1.setAttribute('fill', colorMap[digits[0]]); // this one is always visible
+
+    if ( digits.length == 2 ) {
+      body.setAttribute('fill', '#D9B477');
+      band2_4.setAttribute('fill', colorMap[digits[1]]);
+      mult_4.setAttribute('fill', colorMap[randomMult]);
+      mult_r_4.setAttribute('fill', colorMap[randomMult]);
+
+      band2_4.setAttribute('opacity', 1);
+      mult_4.setAttribute('opacity', 1);
+      mult_r_4.setAttribute('opacity', 0.75);
+
+      if ( randomMult == -1) {
+        mult_r_4.setAttribute('fill', '#ffff33');
+        mult_s_4.setAttribute('opacity', 0.4);
+      } else if ( randomMult == -2) {
+        mult_r_4.setAttribute('fill', '#ffffff');
+        mult_s_4.setAttribute('opacity', 0.4);
+      } else { 
+        mult_s_4.setAttribute('opacity', 0);
+      }
+
+      band2_5.setAttribute('opacity', 0);
+      band3_5.setAttribute('opacity', 0);
+      mult_5.setAttribute('opacity', 0);
+      mult_r_5.setAttribute('opacity', 0);
+      mult_s_5.setAttribute('opacity', 0);
+
+    } else if ( digits.length == 3 ) {
+      body.setAttribute('fill', '#89dde9');
+      band2_5.setAttribute('fill', colorMap[digits[1]]);
+      band3_5.setAttribute('fill', colorMap[digits[2]]);
+      mult_5.setAttribute('fill', colorMap[randomMult]);
+      mult_r_5.setAttribute('fill', colorMap[randomMult]);
+
+      band2_5.setAttribute('opacity', 1);
+      band3_5.setAttribute('opacity', 1);
+      mult_5.setAttribute('opacity', 1);
+      mult_r_5.setAttribute('opacity', 0.75);
+      
+      if ( randomMult == -1) {
+        mult_r_5.setAttribute('fill', '#ffff33');
+        mult_s_5.setAttribute('opacity', 0.4);
+      } else if ( randomMult == -2) {
+        mult_r_5.setAttribute('fill', '#ffffff');
+        mult_s_5.setAttribute('opacity', 0.4);
+      } else { 
+        mult_s_5.setAttribute('opacity', 0);
+      }
+
+      band2_4.setAttribute('opacity', 0);
+      mult_4.setAttribute('opacity', 0);
+      mult_r_4.setAttribute('opacity', 0);
+      mult_s_4.setAttribute('opacity', 0);
+
+    } // else {
+    //   // be sad?
+    // }
+}
+
+// generates a new resistance for question, updates resistor
+function genQuestion() {
+  let question, answer;
+  let numPair = genResistance();
+  updateResistor(numPair);
+  answer = numPair[0] * Math.pow(10, numPair[1]);
+  question = document.getElementById('svg1');
+  return [question, answer]
 }
 
 // posts questions
 function postQuestion(){
   answered = false
   gen = genQuestion();
-  if ((lastQuestion === gen[0]) || (gen[0] == undefined)){
+  if ((lastQuestion == gen[0]) || (gen[0] == undefined)){
     postQuestion();
   };
   qn.textContent = `Question ${questionNumber}`;
-  q.textContent = gen[0];
+  // q.textContent = gen[0];
   s.textContent = `${score}`;
   answer = gen[1];
-  console.log(answer)
+  // console.log(answer);
   a.focus();
   a.click();
   a.addEventListener('keyup', checkAnswer);
@@ -91,17 +234,60 @@ function postQuestion(){
 // checks answer to question (triggered on keyup and recurses postQuestion)
 function checkAnswer(e){
   if(e.keyCode == 13){
+    // console.log("User Input:", a.value); // Log user input
+    // console.log("Correct Answer:", answer); // Log correct answer
+    // console.log("User Input Type:", typeof userInput); // Log type of user input
+    // console.log("Correct Answer Type:", typeof answer); // Log type of correct answer
+    // console.log(gen[0]);
     QNum.push(questionNumber);
-    Q.push(gen[0]);
+    lastQuestion = Q[Q.length - 1];
+    // console.log("last answer:", trueA[trueA.length-1]);
+    Q.push(String(gen[0].innerHTML));
+    // console.log(Q);
+    if (!Number.isInteger(answer)){
+      if (answer < 1){
+        answer = answer.toFixed(2);
+      } else if (answer < 10){
+          if (series == 'e3' || series == 'e6' || series == 'e12'  || series == 'e24'){
+            answer = answer.toFixed(1);
+          } else {
+            answer = answer.toFixed(2);
+          }
+      } else if (answer < 100){
+        answer = answer.toFixed(1);
+      }
+    }
+    // console.log(Number(a.value), a.value, typeof a.value);
+    // if (isNaN(a.value)){
+    //   console.log("not a number");
+    //   var tempUserIn = a.value.replaceAll(' ', '');
+    //   console.log('temp1:', tempUserIn);
+    //   var tempUserIn = tempUserIn.replace('G', '000000000');
+    //   console.log('temp2:', tempUserIn);
+    //   var tempUserIn = tempUserIn.replace('M', '000000');
+    //   console.log('temp3:', tempUserIn);
+    //   var tempUserIn = tempUserIn.replace('k', '000');
+    //   console.log('temp4:', tempUserIn);
+    //   var tempUserIn = a.value.replaceAll('.', '');
+    //   console.log('temp5:', tempUserIn);
+    //   var userIn = tempUserIn;
+    // } else {
+    //   var userIn = a.value;
+    // }
+    // console.log("User Input:", userIn, 'temp:', tempUserIn); // Log user input
+    // console.log("Correct Answer:", answer); // Log correct answer
     userA.push(a.value);
     trueA.push(answer);
+    // console.log("this answer:", trueA[trueA.length-1]);
     if ((Number(a.value) == answer) & (a.value != '')){
       score ++;
       isTrue.push(true)
+      // console.log('correct!');
     }
     else if (Number(a.value) != answer){
       wrong ++;
       isTrue.push(false)
+      // console.log('incorrect :(');
     };
     a.removeEventListener('keyup', checkAnswer);
     a.value = '';
@@ -119,7 +305,7 @@ function victory(){
   sessionStorage.setItem('wrong', wrong);
   sessionStorage.setItem('timer', `${convertToString(hour)}:${convertToString(minute)}:${convertToString(second)}`);
   sessionStorage.setItem('QNum', QNum);
-  sessionStorage.setItem('Q', Q);
+  sessionStorage.setItem('Q', Q.join("%%%"));
   sessionStorage.setItem('userA', userA);
   sessionStorage.setItem('trueA', trueA);
   sessionStorage.setItem('isTrue', isTrue);
@@ -144,11 +330,11 @@ function addTime(){
   second++;
   if (second === 60){
     minute++;
-    second = 00
+    second = 0
   }
   if (minute === 60){
     hour++;
-    minute = 00
+    minute = 0
   }
   hr.textContent = convertToString(hour);
   min.textContent = convertToString(minute);
